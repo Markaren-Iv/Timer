e1a7bcb88cbd5d19d58169672e2d25c1236893d2
Ivan Petryk <ivanlark0@gmail.com>
2025-11-12 09:36:35 +0200

    set time update

--- main.py
+++ main.py
@@ -11,22 +11,30 @@ frame1.pack()
 label = tk.Label(frame1, text="Shutdown timer")
 label.pack()
 
-current_time = time.localtime()
-hour = str(current_time.tm_hour)
-minutes = str(current_time.tm_min)
-seconds = str(current_time.tm_sec)
-
 frame2 = tk.Frame(window, relief="solid")
 frame2.pack(pady=10)
-hours_label = tk.Label(frame2, text = hour + " : ")
-hours_label.grid(row=2, column=0, padx=0, pady=0)
+hours_label = tk.Label(frame2, text = "")
+hours_label.grid(row=2, column=0)
 
-minutes_label = tk.Label(frame2, text = minutes + " : ")
-minutes_label.grid(row=2, column=1, padx=0, pady=0)
+minutes_label = tk.Label(frame2, text = "")
+minutes_label.grid(row=2, column=1)
 
-seconds_label = tk.Label(frame2, text = seconds)
-seconds_label.grid(row=2, column=2, padx=0, pady=0)
+seconds_label = tk.Label(frame2, text = "")
+seconds_label.grid(row=2, column=2)
 
+def update_time():
+    current_time = time.localtime()
+    hour = str(current_time.tm_hour)
+    minutes = str(current_time.tm_min)
+    seconds = str(current_time.tm_sec)
+
+    hours_label.config(text = hour + " : ")
+    minutes_label.config(text = minutes + " : ")
+    seconds_label.config(text = seconds) 
+
+    window.after(1000, update_time)
+
+update_time()
 
 window.mainloop()
 
